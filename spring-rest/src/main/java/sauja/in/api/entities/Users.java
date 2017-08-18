597a2762ac0471967cb7ecc7b933eba65288dbb3
Saurabh Jadhav <sauja7@gmail.com>
2017-08-17 20:19:46 -0500

    feat(spring-rest): add lombox depedency
    lombok

--- spring-rest/src/main/java/sauja/in/api/entities/Users.java
+++ spring-rest/src/main/java/sauja/in/api/entities/Users.java
@@ -1,43 +1,13 @@
 package sauja.in.api.entities;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Users {
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String email;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private @Getter @Setter String id;
+    private @Getter @Setter String firstName;
+    private @Getter @Setter String lastName;
+    private @Getter @Setter String email;
 }
